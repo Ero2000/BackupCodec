2a2aebbaa9b46e0711570767c59f44caddd246f9
Ero2000 <ero@stuy.edu>
2017-03-22 13:58:16 -0400

    Update Latkes.java

--- Latkes.java
+++ Latkes.java
@@ -14,31 +14,38 @@ public class Latkes
     //constructor
     public Latkes(int size) 
     { 
-	/* YOUR IMPLEMENTATION HERE */
+	_stack = new String[size];
+	_stackSize = 0;
     }
 
     //means of insertion
     public void push( String s ) 
     {
-	/* YOUR IMPLEMENTATION HERE */
+	_stack[_stackSize+1] = s;
+	_stackSize++;
     }
 
     //means of removal
     public String pop( ) 
     { 
-	/* YOUR IMPLEMENTATION HERE */
+	_stack[_stackSize] = null;
+	_stackSize--;
     }
 
     //chk for emptiness
     public boolean isEmpty() 
     { 
-	/* YOUR IMPLEMENTATION HERE */
+	if (_stackSize == 0)
+		return true;
+	return false;	
     }
 
     //chk for fullness
     public boolean isFull() 
     {
-	/* YOUR IMPLEMENTATION HERE */
+	if (_stackSize == _stack.length)
+		return true;
+	return false;
     }
 
 }//end class Latkes
